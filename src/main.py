5e25da713bd7256dd94b4238fd55ae7f1e8debd3
Eduardo Verissimo <verissimo_eduardo@hotmail.com>
2023-07-21 19:29:15 -0300

    Acesso a resposta do GPT arrumada

--- src/main.py
+++ src/main.py
@@ -2,27 +2,13 @@ import openai
 
 openai.api_key = "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"
 
-
 # Função para interagir com o modelo e obter a resposta do chatbot
 def obter_resposta(mensagens):
     try:
         # Fazer uma chamada à API para obter a resposta do modelo GPT-3.5-turbo com as mensagens de entrada
         resposta = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Você é um assistente útil.",
-                },  # Você é um assistente útil.
-                {
-                    "role": "system",
-                    "content": "Você será o atendente de uma loja de eletrônicos (pode assumir que todos os itens existem e dar valores ficticios aos mesmos.) ",
-                },
-                {
-                    "role": "user",
-                    "content": "Qual o preco do Iphone",
-                },  # O cliente pergunta algo.
-            ],
+            messages=mensagens,
             temperature=1,
             max_tokens=256,
             top_p=1,
@@ -30,7 +16,7 @@ def obter_resposta(mensagens):
             presence_penalty=0,
         )
 
-        return resposta.choices[0].text.strip()
+        return resposta.choices[0].message.content.strip()
 
     except Exception as e:
         # Lidar com exceções se ocorrerem erros durante a interação com a API
@@ -50,17 +36,9 @@ def atendimento_ao_cliente():
             break
 
         # Definir as mensagens para a API do ChatCompletion
-        mensagens = [
-            {
-                "role": "system",
-                "content": "Você é um assistente útil.",
-            },  # Você é um assistente útil.
-            {
-                "role": "system",
-                "content": "Você será o atendente de uma loja de eletrônicos (pode assumir que todos os itens existem e dar valores ficticios aos mesmos.) ",
-            },
-            {"role": "user", "content": pergunta},  # O cliente pergunta algo.
-        ]
+        mensagens = [{"role": "system","content": "Você é um assistente útil."}, 
+         {"role": "system","content": "Você será o atendente de uma loja de eletrônicos (pode assumir que todos os itens existem e dar valores ficticios aos mesmos.)"},
+         {"role": "user", "content": pergunta}]
 
         # Obter a resposta do chatbot com base na pergunta do cliente
         resposta = obter_resposta(mensagens)
